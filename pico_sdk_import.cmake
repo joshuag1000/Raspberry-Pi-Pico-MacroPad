11c6f30816d5b10367309ef228f4019e1f4815e6
Joshua Glass <joshuag1000@outlook.com>
2022-06-04 00:45:18 +0100

    Removed typo

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -1,7 +1,7 @@
 # This is a copy of <PICO_SDK_PATH>/external/pico_sdk_import.cmake
 
 # This can be dropped into an external project to help locate this SDK
-# It should be include()ed prior to project()
+# It should be includeed prior to project()
 
 # todo document
 
